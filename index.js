9e1a6bcf994a791743b7f02d0ff814f217949c13
vwt12eh8 <63479906+vwt12eh8@users.noreply.github.com>
2020-09-13 10:52:24 +0900

    To bindings

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-'use strict';
+const events = require('events');
+const util = require('util');
 
-const os = require('os');
+const NobleWinrt =  require('./build/Release/noble_winrt.node').NobleWinrt;
 
-// noble-winrt acts as a shim to noble.
-if (os.platform() === 'win32') {
-	const ver = os.release().split('.').map(Number);
-	if (!(ver[0] > 10 ||
-		(ver[0] === 10 && ver[1] > 0) ||
-		(ver[0] === 10 && ver[1] === 0 && ver[2] >= 15063))) {
-		module.exports = require('noble');
-	} else {
-		const Noble = require('noble/lib/noble');
-		const winrtBindings = require('./lib/binding.js');
-		var nobleInstance = new Noble(winrtBindings);
-		module.exports = nobleInstance;
-	}
-} else {
-	module.exports = require('noble');
-}
+util.inherits(NobleWinrt, events.EventEmitter);
+
+module.exports = NobleWinrt;
